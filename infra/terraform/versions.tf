a21da5bbac6abb902361c6a578a3ffe93db58fef
AndreToral <jrodriguez16@upao.edu.pe>
2025-11-28 11:20:15 -0500

    feat(config): Configurar backend S3, bloqueo DynamoDB y proveedor 'random'

--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -1,10 +1,23 @@
 // Configuración de versiones y proveedores requeridos
 terraform {
   required_version = ">= 1.5.0"
+  backend "s3" {
+    bucket         = "regaloshop-tfstate"
+    key            = "regaloshop/terraform.tfstate"
+    # Si usas workspaces, puedes habilitar:
+    # workspace_key_prefix = "regaloshop"
+    region         = "us-east-1"
+    dynamodb_table = "regaloshop-tflock"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.0"
     }
   }
 }
